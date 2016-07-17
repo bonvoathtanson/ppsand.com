8d5a42de54fbbd4dff185a5909a7c08370cac0fb
ppsand.com <bonvoathit@gmail.com>
2016-07-17 20:37:53 +0700

    comment

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,10 +3,15 @@
   <head>
     <meta charset="utf-8">
     <title>ប្រព័ន្ធគ្រប់គ្រងខ្សាច់</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{url('css/base.css')}}" media="screen" title="no title" charset="utf-8">
     @yield('css')
   </head>
   <body>
     @yield('content')
   </body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js" charset="utf-8"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" crossorigin="anonymous"></script>
   @yield('script')
 </html>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,7 +3,24 @@
 
 @endsection
 @section('content')
-    <p>This is my body content.</p>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="" style="padding:5px;">
+          <a href="#" class="btn btn-primary btn-sm">បន្ថែម</a>
+        </div>
+        <table class="table table-bordered table-hover">
+          <thead>
+            <tr class="active">
+              <th>ល.រ</th>
+              <th>លេខកូដ</th>
+              <th>ឈ្មោះទំនិញ</th>
+              <th>តំលៃលក់</th>
+              <th>ចំនួនក្នុងស្តុក</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
 @endsection
 
 @section('script')
