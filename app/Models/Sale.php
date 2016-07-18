698629cedaffb8895cf271da0d6fb704348a2de3
bonvoathit@gmail.com <bonvoathit@gmail.com>
2016-07-18 11:08:30 +0700

    comment

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Expanse extends Model
+class Sale extends Model
 {
     //
 }
--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Income extends Model
+class Supplier extends Model
 {
     //
 }
